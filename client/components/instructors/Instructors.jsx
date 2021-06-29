c783aa160870e34b6cf70fd78f56de9b8f07e952
jsmithb117 <jsmithb117@gmail.com>
2021-06-29 13:22:55 -0700

    GraphQL fetches data from server.

--- client/components/instructors/Instructors.jsx
+++ client/components/instructors/Instructors.jsx
@@ -56,6 +56,26 @@ const Instructors = () => {
       .then((response) => response.json())
       .then((json) => { setSvgs(json); })
       .catch((err) => { if (err) { console.error(err); } });
+
+    const dice = 3;
+    const sides = 6;
+    const query = `query RollDice($dice: Int!, $sides: Int) {
+  rollDice(numDice: $dice, numSides: $sides)
+}`;
+
+    fetch('/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables: { dice, sides },
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => console.log('data returned:', data));
   }, []);
 
   return (
--- server/index.js
+++ server/index.js
@@ -19,10 +19,10 @@ const schema = buildSchema(
 );
 
 const root = {
-  rollDice: (args) => {
+  rollDice: ({ numDice, numSides }) => {
     const output = [];
-    for (let i = 0; i < args.numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
     }
     return output;
   },
